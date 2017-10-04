e0ca9ec7c5ec7fe1795a30372fc06a18831ee085
waltnore <waltnore@gmail.com>
2017-10-04 00:12:47 -0500

    commint desde casa con grupos bien con tabla

--- src/app/items/grupos/grupos.component.ts
+++ src/app/items/grupos/grupos.component.ts
@@ -10,20 +10,28 @@ export class GruposComponent   {
   newgrupo:boolean;
  
 
-tableData1 = {
-    headerRow: [ '#', 'Name', 'Job Position', 'Since', 'Salary', 'Actions'],
-    dataRows: [
+tableData1 =  [
         ['1', 'Andrew Mike', 'Develop', '2013', '99,225',''],
         ['2', 'John Doe', 'Design', '2012', '89,241', 'btn-round'],
         ['3', 'Alex Mike', 'Design', '2010', '92,144', 'btn-simple'],
         ['4','Mike Monday', 'Marketing', '2013', '49,990', 'btn-round'],
-        ['5', 'Paul Dickens', 'Communication', '2015', '69,201', '']
-    ]
- };
+        ['5', 'Paul Dickens', 'Communication', '2015', '69,201', ''],
+        ['6', 'Paul Dickens', 'Communication', '2015', '69,201', '']
+    ];
 
   CreadoGrupo(){
-    this.newgrupo=true
-    console.log("dio clic")
+    this.newgrupo=true;
+    console.log("dio clic");
+  }
+
+  addItem(){
+    this.tableData1.push( ['7', 'Walter', 'Prueba', '2017', '99,225','']);
+  }
+
+  borrarItem(){
+
+    this.tableData1.splice(1, 1);
+    
   }
   
 }
